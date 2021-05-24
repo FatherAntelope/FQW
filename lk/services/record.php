37d46728907617deac04ff5f0d97fdce7a66cc7d
FatherAntelope <43047846+FatherAntilopi@users.noreply.github.com>
2021-05-24 23:10:55 +0500

    Добавлены проверки доступа к модулям пациента

--- lk/services/record.php
+++ lk/services/record.php
@@ -1,4 +1,17 @@
 <?php
+if (!isset($_COOKIE['user_token']))
+    header("Location: /error/401.php");
+require $_SERVER['DOCUMENT_ROOT'] . "/utils/User.php";
+$user = new User($_COOKIE['user_token']);
+if ($user->getUserStatusCode() === 400 || $user->getUserStatusCode() === 403) {
+    setcookie('user_token', '', 0, "/");
+    header("Location: /error/401.php");
+}
+if (!$user->isUserRole("Patient"))
+    header("Location: /error/403.php");
+
+$user_data = $user->getUserData();
+
 $whose_user = 2;
 if ($_GET['type'] == 'doctor')
     require $_SERVER['DOCUMENT_ROOT']."/lk/services/record_doctor.php";
